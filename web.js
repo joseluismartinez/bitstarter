cab288586a340b10d995aa92709e2068bb927a17
joseluismartinez <joseluis.martinezbustos@gmail.com>
2013-07-10 01:13:51 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
-var express = require('express');
+vvar express = require('express');
+var fs = require("fs");
 
-var app = express.createServer(express.logger());
-
+var app = express(express.logger());
+var bf = new  Buffer(fs.readFileSync("./index.html"));
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(bf);
 });
 
 var port = process.env.PORT || 5000;
